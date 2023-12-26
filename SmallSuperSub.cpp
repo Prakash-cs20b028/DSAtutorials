73d9a90df16862730778706601945bda241fc684
Prakash-cs20b028 <Programmer52525@gmail.com>
2023-12-26 22:47:27 +0530

    Smallest super subsequence solution has been added

---
+++ SmallSuperSub.cpp
@@ -0,0 +1,36 @@
+ 
+int fun(string s1,string s2,int m,int n)
+    {
+        int t[m+1][n+1];
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0)
+                {
+                    t[i][j] = 0;
+                }
+            }
+        }
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else
+                {
+                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        return m+n-t[m][n];
+        
+    }
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        return fun(X,Y,m,n);
+    }
